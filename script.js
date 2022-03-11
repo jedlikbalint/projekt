5ef83d1fb8c61647e924ce947fd0c2fdd8d3340c
holperj <holper.julia@students.jedlik.eu>
2022-03-11 08:44:01 +0100

    mehet gomb (nem müködik)

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function kuld() {
     document.getElementById("kuldgomb").innerHTML = "Köszönjük , hamarosan kapcsolatba lépünk önnel !";
   }
+  function mehet() {
+    document.getElementById("mehetgomb").innerHTML = "Sikeres bejenlentkezés!";
+  }
+    
